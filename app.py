1b3aec823dcfb409cd07cf3b752d474e2648a084
Mohith Gowda K <mohithgowdak.kote@gmail.com>
2024-08-06 17:36:37 +0530

    after updation

--- app.py
+++ app.py
@@ -179,20 +179,26 @@ def counsellor():
     if g.user:
         conn = sqlite3.connect(database)
         c = conn.cursor()
-        c.execute("SELECT * FROM users WHERE id = '"+str(session['user'])+"'")
-        profile = c.fetchone()
-
-        c.execute("SELECT * FROM appointments WHERE isbooked = '0'")
-        requests = c.fetchall()
-
-        c.execute("SELECT * FROM appointments WHERE date >= CURRENT_DATE AND isbooked = '1' AND counsellor = '"+str(session['user'])+"'")
-        scheduled = c.fetchall()
-
-        c.execute("SELECT COUNT(username) FROM appointments WHERE date < CURRENT_DATE AND counsellor = '"+str(session['user'])+"'")
-        taken = c.fetchall()
-
-        c.execute("SELECT COUNT(username) FROM appointments WHERE date >= CURRENT_DATE AND counsellor = '"+str(session['user'])+"'")
+        
+        # Get scheduled_no
+        c.execute("SELECT COUNT(username) FROM appointments WHERE date >= CURRENT_DATE AND counsellor = ?", (str(session['user']),))
         scheduled_no = c.fetchall()
+        
+        # Get taken
+        c.execute("SELECT COUNT(username) FROM appointments WHERE date < CURRENT_DATE AND counsellor = ?", (str(session['user']),))
+        taken = c.fetchall()
+        
+        # Get scheduled in reverse chronological order
+        c.execute("SELECT * FROM appointments WHERE date >= CURRENT_DATE AND isbooked = '1' AND counsellor = ? ORDER BY date DESC, time DESC", (str(session['user']),))
+        scheduled = c.fetchall()
+        
+        # Get requests in reverse chronological order
+        c.execute("SELECT * FROM appointments WHERE isbooked = '0' ORDER BY date DESC, time DESC")
+        requests = c.fetchall()
+        
+        # Get profile
+        c.execute("SELECT * FROM users WHERE id = ?", (str(session['user']),))
+        profile = c.fetchone()
         
         context = {
             'profile': profile,
